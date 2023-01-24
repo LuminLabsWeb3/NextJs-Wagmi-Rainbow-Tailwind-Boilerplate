95a3a08e37835378bc318323e0f5a0411a4557e7
yusufferdogan <yusuffx03@gmail.com>
2023-01-24 18:40:18 +0300

    refactor home

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,16 +4,14 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Body from "./body";
 
-import { ConnectButton } from "@rainbow-me/rainbowkit";
-
 const Home = () => {
   return (
-    <div>
+    <>
       <CustomHead></CustomHead>
       <Header></Header>
       <Body></Body>
       <Footer></Footer>
-    </div>
+    </>
   );
 };
 
